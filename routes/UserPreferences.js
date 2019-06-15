3d6af8a1dffe7c968e4d0d9fff8faecb38ad9d5c
Lucian Ghimpu <Lucian.Ghimpu@Yardi.Com>
2019-06-15 20:23:21 +0300

    forgot await

--- routes/UserPreferences.js
+++ routes/UserPreferences.js
@@ -7,7 +7,7 @@ const checkAuth = require('../middleware/check-auth')
 router.patch('/', checkAuth, async (req, res, next) => {
 
     try {
-        const user = User.update({_id: req.body._id}, { $set: req.body })
+        const user = await User.findByIdAndUpdate({_id: req.body._id}, { $set: req.body }, {new: true})
         return res.status(200).json(user)
     } catch (e) {
         return res.status(500).json(e)
