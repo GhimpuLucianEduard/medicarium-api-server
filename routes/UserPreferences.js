7ffe9efacb5708d2d4c6e6344f600b0c02f9cc4f
Lucian Ghimpu <Lucian.Ghimpu@Yardi.Com>
2019-06-15 19:55:07 +0300

    ops

--- routes/UserPreferences.js
+++ routes/UserPreferences.js
@@ -7,7 +7,7 @@ const checkAuth = require('../middleware/check-auth')
 router.patch('/', checkAuth, async (req, res, next) => {
 
     try {
-        const user = User.update({_id: req.body._id})
+        const user = User.update({_id: req.body._id}, { $set: req.body })
         return res.status(200).json(user)
     } catch (e) {
         return res.status(500).json(e)
